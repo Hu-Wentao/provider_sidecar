4da106e13e7b35732018c658558d228ee857e13d
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2022-08-14 12:40:48 +0800

    feat utils.dart findLineIndexBy

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,9 +1,28 @@
-/// 返回[StackTrace]特定一行的内容
-String? selectLineAt(String trace, int line) {
-  final reg = RegExp("#$line" r'\s*(.*)\n');
+///
+/// 返回[StackTrace]特定行号[lineIndex] #n 的内容
+String? selectLineAt(String trace, int lineIndex) {
+  final reg = RegExp("#$lineIndex" r'\s*(.*)\n');
   final m = reg.allMatches(trace).first;
   return m.group(1);
 }
+
+///
+/// 返回[StackTrace]包含特定内容[$name]的行号
+int? findLineIndexBy(String trace, String content) {
+  final reg = RegExp(r'#(\d+)\s*.*' "$content" r'.*\n');
+  final m = reg.allMatches(trace).first;
+  final r = m.group(1);
+  return r == null ? null : int.tryParse(r);
+}
+
 extension StaceTraceX on StackTrace {
-  lineAt(int line) => selectLineAt(toString(), line);
+  String? lineAt(int line) => selectLineAt(toString(), line);
+
+  int? lineIndexBy(String content) => findLineIndexBy(toString(), content);
+
+  String? parentLineBy(String content) {
+    final trace = StackTrace.current.toString();
+    final index = findLineIndexBy(trace, content);
+    return index == null ? null : selectLineAt(trace, index + 1);
+  }
 }

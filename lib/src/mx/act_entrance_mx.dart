f556ab9cd555aad9dd85cd23bb7f5586682063ea
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2022-09-27 23:04:26 +0800

    fix act_entrance_mx.dart 修复缺少范型问题

--- lib/src/mx/act_entrance_mx.dart
+++ lib/src/mx/act_entrance_mx.dart
@@ -2,9 +2,10 @@ part of 'mx.dart';
 
 ///
 /// Act入口包装
-mixin ActEntranceMx<ACT, EX> on Sidecar<dynamic, EX> {
+mixin ActEntranceMx<ACT, S, EX> on Sidecar<S, EX> {
   Future<EX?> actEntrance(ACT act) async => await actWrapper(
-      () async => onBeforeActEntrance(act) ?? await onActEntrance(act));
+        () async => onBeforeActEntrance(act) ?? await onActEntrance(act),
+      );
 
   // 自定义防抖节流
   EX? onBeforeActEntrance(ACT act) => null;
